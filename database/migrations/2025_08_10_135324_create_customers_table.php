724ff6c118fe1390a0b4e34f1679335c2d10370e
ilhamizzul <ilhamizzul@gmail.com>
2025-08-10 21:13:50 +0700

    Create migration for customers table with necessary fields

---
+++ database/migrations/2025_08_10_135324_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 255)->unique();
+            $table->string('phone', 12)->unique();
+            $table->string('account_number', 20);
+            $table->text('about')->nullable();
+            $table->string('image')->default('/default-images/ben.png');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
